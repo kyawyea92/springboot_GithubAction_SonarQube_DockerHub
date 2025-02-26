fc7ff2949db0b58e6614175fedec1b8e4da7ce1e
Kyaw Ye Lwin <kyawyealwin2017@gmail.com>
2025-02-26 14:19:19 +0700

    Initital Commit

---
+++ SpringBootSonarQube/src/main/java/com/springboot/sonarqube/SpringBootSonarQubeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonarQubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSonarQubeApplication.class, args);
+	}
+
+}
